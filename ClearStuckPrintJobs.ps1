784d87b3661b5ab0645ca44d388bc899bab7a4e3
Connor (WizardTux) <connor@itbible.org>
2022-12-10 11:48:39 -0600

    Initial commit of files.

---
+++ ClearStuckPrintJobs.ps1
@@ -0,0 +1,32 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created on:   	12/9/2022 11:38 PM
+	 Created by:   	WizardTux
+	 Organization: 	IT Bible (itbible.org)
+	 Filename: ClearStuckPrintJobs.ps1     	
+	===========================================================================
+	.DESCRIPTION
+		Clears print jobs on an array of computers
+#>
+
+$Computers = @(
+	"."	
+)
+
+foreach ($Computer in $Computers)
+{
+	$name = $Computer
+	if ($Computer -eq ".") { $name = "Local Machine" }
+	Write-Host "Checking Print Jobs on: $($name)"
+	$PrintJobs = Get-WmiObject -Class "Win32_PrintJob" -Namespace "root\CIMV2" -ComputerName $Computer
+	foreach ($job in $PrintJobs)
+	{
+		$pos = ($job.Name).IndexOf(",")
+		$printerName = ($job.Name).Substring(0, $pos)
+		if ($job.JobStatus -like "Error | Printing")
+		{
+			Remove-PrintJob -ComputerName $env:COMPUTERNAME -ID $job.JobId -PrinterName $printerName
+		}
+	}
+}
---
+++ GetProcessByPort.ps1
@@ -0,0 +1,29 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created on:   	12/10/2022 12:45 AM
+	 Created by:   	WizardTux
+	 Organization: 	IT Bible (itbible.org)
+	 Filename: GetProcessByPort.ps1     	
+	===========================================================================
+	.DESCRIPTION
+		Gets a list of processes using a specific port and outputs the list.
+#>
+
+param (
+	[Parameter(Mandatory = $true)]
+	[string]$Port # e.g. 443
+)
+
+$pids = (Get-NetTCPConnection | Where-Object { $_.LocalPort -eq $Port }) | Select-Object -Property OwningProcess
+
+$lastName=""
+foreach ($localpid in $pids)
+{
+	$current = Get-Process -PID $localpid.OwningProcess | Select-Object -Property ProcessName, Id
+	if ($current.ProcessName -ne $lastName)
+	{
+		$current
+		$lastName = $current.ProcessName
+	}
+}
